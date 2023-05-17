1a0c0fd5058ae3436e5bdfa846cf43d62e845be9
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-17 16:03:06 +0200

    add AppTitle in components

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+	<h1>La mia prima app con Vite!</h1>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate similique nobis saepe a earum facere, eius
+		quibusdam, ratione distinctio, ipsam quisquam odio culpa cupiditate. Explicabo qui veniam tenetur corrupti eum!</p>
+</template>
+
+<style scoped></style>
